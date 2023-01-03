4223ad39b1623a57915f424ee9763203ffaf41c2
rathiharshit123 <53534753+rathiharshit123@users.noreply.github.com>
2023-01-04 01:57:15 +0530

    app changes

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,6 @@ app.use(express.json());
 const port = 3000;
 const route= require("./server/routes");
 
-const {connection} = require("./server/mongo-connection")
 
 app.listen(port, ()=>{
     console.log(chalk.green(`listeting on port :${port}`))
